e2e03d2fed0cf289e6cc023b00236701aedeffdb
kevinMora47 <kevinmora47@gmail.com>
2021-12-14 20:21:56 -0600

    feat: :sparkles: Models Creation
    Products and Cart models definition

---
+++ src/models/ProductModel.js
@@ -0,0 +1,36 @@
+const Products = [
+  {
+    id: 1,
+    timestamp: new Date(),
+    name: 'Keychron K2',
+    description:
+      'A 75% layout (84-key) white LED backlight compact Bluetooth mechanical keyboard. The ultimate keyboard that retains shortcut and arrow keys. Inclined bottom frame makes the K2 more comfortable to type even without a palm rest.',
+    code: '1',
+    photo: 'https://picsum.photos/200/300',
+    price: '$79.99',
+    stock: '10',
+  },
+  {
+    id: 2,
+    timestamp: new Date(),
+    name: 'Razer Viper Ultimate',
+    description:
+      '25 percentage Quicker Than Competing Wireless Mice: Razer HyperSpeed wireless technology brings together extreme low-latency and interference reduction for true wireless freedom ',
+    code: '2',
+    photo: 'https://picsum.photos/200/300',
+    price: '$69.99',
+    stock: '10',
+  },
+  {
+    id: 3,
+    timestamp: new Date(),
+    name: 'Cracking the Coding Interview',
+    description:
+      'Her background is in software development. She has worked as a software engineer at Google, Microsoft, and Apple. At Google, she interviewed hundreds of software engineers and evaluated thousands of hiring packets on the hiring committee. She holds a B.S.E. and M.S.E. in computer science from the University of Pennsylvania and an MBA from the Wharton School. ',
+    code: '3',
+    photo: 'https://picsum.photos/200/300',
+    price: '24.49',
+    stock: '10',
+  },
+];
+module.exports = Products;
---
+++ src/models/ShoppingCartModel.js
@@ -0,0 +1,17 @@
+const ShoppingCart = [
+  {
+    id: 1,
+    cart: [
+      {
+        timestamp: new Date(),
+        name: 'Product 1',
+        description: 'Product',
+        code: '1',
+        photo: 'https://picsum.photos/200/300',
+        price: '100',
+        stock: '10',
+      },
+    ],
+  },
+];
+module.exports = ShoppingCart;
