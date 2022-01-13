5f3a3cc79a4792c9984c926816247f0c6af0276b
kevinMora47 <kevinmora47@gmail.com>
2022-01-12 20:19:10 -0600

    feat: :art: ESLint
    Added of new rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,24 @@
 module.exports = {
-  extends: ['airbnb', 'prettier', 'plugin:node/recommended'],
+  extends: [
+    'airbnb',
+    'prettier',
+    'plugin:node/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
   plugins: ['prettier'],
   rules: {
     'no-unused-vars': 'warn',
     'no-console': 'off',
     'func-names': 'off',
     'class-methods-use-this': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   },
 };
